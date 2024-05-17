99581768bfe4b34c538f5812aa7ee0a2b26fd925
Syed Hassan Bukhari <162698882+Hassansyed7@users.noreply.github.com>
2024-05-18 01:09:09 +0500

    Add files via upload

---
+++ Q42/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// 42. Great Magicians: Start with a copy of your program from Exercise 39.
+// Write a function called make_great() that modifies the array of magicians by adding
+// the phrase the Great to each magician’s name. Call show_magicians() to
+// see that the list has actually been modified.
+Object.defineProperty(exports, "__esModule", { value: true });
+const magicians = ["Penn Jillette", "Criss Angel", "Teller"];
+function showMagicians(magicians) {
+    console.log(magicians.join("\n"));
+}
+function makeGreat(magicians) {
+    return magicians.map((magician) => `${magician} the Great`);
+}
+console.log("Original magicians:");
+showMagicians(magicians);
+const greatMagicians = makeGreat(magicians);
+console.log("\nModified list:");
+showMagicians(greatMagicians);
